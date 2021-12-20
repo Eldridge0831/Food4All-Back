762fb81d3c894f95b64824f12636e189ddae95db
tjohns823 <tjohns823@gmail.com>
2021-12-20 16:46:33 -0500

    for testing purposes

--- app.js
+++ app.js
@@ -1,186 +1,132 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require("cors");
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || "production";
+const config = require("./config/config.json")[env];
+const db = {};
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const res = require("express/lib/response");
+const FormData = require("form-data");
 
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+app.use(cors({ origin: (orig, cb) => cb(null, true), credentials: true }));
 
-// database requirements
-const { sequelize, Model, dataTypes } = require("sequelize");
-const users = require("./models").users;
-const favorites = require("./models").favorites;
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testAPIRouter = require("./routes/testAPI");
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(cors());
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(bodyParser.json());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use("/testAPI", testAPIRouter);
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(express.static(path.join(__dirname, "templates")));
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js";
+  })
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+module.exports = db;
+
+class Users extends Model {}
+users.init(
+  {
+    userName: DataTypes.STRING,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+  },
+  {
+    sequelize,
+    modelName: "Users",
+  }
+);
+// add a user   
+app.post("/users", async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  await Users.create({
+    userName: req.body.userName,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    userImage: req.body.userImage,
+  });
+  res.send('{"userRegistered": "true"}');
+});
+// get all users
+app.get("/users", async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  const users = await Users.findAll();
+  res.status(200).send(users);
 });
 
-let user = {};
-
-passport.serializeUser((user, cb) => {
-  cb(null, user);
+// get one user
+app.get("/users/:email", async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  let email = req.params.email;
+  const users = await Users.findAll({
+    where: {
+      email: email,
+    },
+  });
+  res.status(200).send(users);
+});
+// update a user   WORKING
+app.put("/users/modify/:user_name", async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  let UserName = req.params["user_name"];
+  await Users.update(
+    {
+      userName: req.body.userName,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+    },
+    {
+      where: {
+        userName: UserName,
+      },
+    }
+  );
+  res.send('{"userRegistered": "true"}');
 });
 
-passport.deserializeUser((user, cb) => {
-  cb(null, user);
+// delete a user   WORKING
+app.delete("/users/delete/:id", async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  let id = req.params["id"];
+  await Users.destroy({
+    where: {
+      id: id,
+    },
+  });
+  res.send('{"userDeleted": "true"}');
 });
-
-/*------Google Passport-------*/
-
-// passport.use(
-//   new GoogleStrategy(
-//     {
-//       clientID: keys.GOOGLE.clientID,
-//       clientSecret: keys.GOOGLE.clientSecret,
-//       callbackURL: "/auth/google/callback",
-//     },
-//     (accessToken, refreshToken, profile, cb) => {
-//       console.log(chalk.blue(JSON.stringify(profile)));
-//       user.findorCreate = { ...profile };
-//       return cb(null, profile);
-//     }
-//   )
-// );
-
-// path to start the OAuth
-// app.get(
-//   "/auth/google",
-//   passport.authenticate("google", {
-//     scope: ["profile", "email"],
-//   })
-// );
-
-// // OAuth callback url
-// app.get(
-//   "/auth/google/callback",
-//   passport.authenticate("google"),
-//   (req, res) => {
-//     res.redirect("/"); //redirect to home if successful
-//   }
-// );
-// app.get("/user", (req, res) => {
-//   console.log("getting user data!");
-//   res.send(user);
-// });
-
-// // Add a User
-// app.post("/user", async (req, res) => {
-//   res.setHeader("Content-Type", "application/json");
-//   await Users.create({
-//     name: req.body.name,
-//     email: req.body.email,
-//   });
-//   res.send('{"userRegistered": "true"}');
-// });
-
-// // Login a User
-// app.post("/loginAttempt", async (req, res) => {
-//   res.setHeader("Content-Type", "application/json");
-//   const username = req.body.username;
-//   console.log(username);
-//   const password = req.body.password;
-//   console.log(password);
-//   Users.findOne({
-//     where: {
-//       userName: username,
-//     },
-//   }).then((users) => {
-//     console.log(users);
-//     bcrypt.compare(password, users.password, function (err, isMatch) {
-//       //bcrypt
-//       if (err) {
-//         throw err;
-//       } else if (!isMatch) {
-//         console.log("isMatch is False");
-//         return res.send('{"isMatch": "false"}');
-//       } else {
-//         console.log("isMatch is True");
-//         res.send('{"isMatch": "true"}');
-//       }
-//     });
-//   });
-// });
-
-// //Logout the User
-// app.get("/auth/logout", (req, res) => {
-//   console.log("logging out!");
-//   user = {};
-//   res.redirect("/"); //redirect to home page
-// });
-
-// // see favorites in DB
-// app.get("/favorite", async (req, res) => {
-//   const favorite = await favorites.findall();
-//   console.log("favorties in DB: ", favorite);
-//   res.status(200).send(JSON.stringify(favorite));
-// });
-
-// // To Update a User -> change it for favorites
-// app.put("/favorite/modify/:id", async (req, res) => {
-//   res.setHeader("Content-Type", "application/json");
-//   let favorites = req.params["id"];
-//   if (!err) {
-//     favorites.update(
-//       {
-//         id: req.body.id,
-//         commentSection: req.body.commentSection,
-//         category: req.body.category,
-//       },
-//       {
-//         where: {
-//           favorites: favorites,
-//         },
-//       }
-//     );
-//   }
-//   res.send('{"faovritesUpdated": "true"}');
-// });
-
-// // Delete a User -> change it for favorites
-// app.delete("/favorite/:id", async (req, res) => {
-//   res.setHeader("Content-Type", "application/json");
-//   let id = req.params["id"];
-//   await favorites.destroy({
-//     where: {
-//       id: id,
-//     },
-//   });
-//   res.send('{"favoritesDeleted": "true"}');
-// });
-
-
-module.exports = app;
